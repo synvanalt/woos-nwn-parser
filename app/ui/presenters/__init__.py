2c55857be402f91c09962a806ebc518a70ce0261
synvanalt <andysonic@gmail.com>
2026-03-22 01:15:40 +0200

    refactor: extract death snippet presenter from widget

---
+++ app/ui/presenters/__init__.py
@@ -0,0 +1,17 @@
+"""Pure presenter helpers for Tk widgets."""
+
+from .death_snippet_presenter import (
+    PreparedDeathSnippetRender,
+    PreparedLine,
+    TextSpan,
+    format_death_event_dropdown_value,
+    prepare_death_snippet_render,
+)
+
+__all__ = [
+    "PreparedDeathSnippetRender",
+    "PreparedLine",
+    "TextSpan",
+    "format_death_event_dropdown_value",
+    "prepare_death_snippet_render",
+]
